d139b410d5cf69c589701107ba5cf6c4ff651aa8
gokugml <32604536+gokugml@users.noreply.github.com>
2018-04-14 11:53:52 -0700

    support store and use ems credential

--- itu_deadlines.py
+++ itu_deadlines.py
@@ -1,9 +1,40 @@
 import data_extraction
 import calendar_adder
 import getpass
+import os
+import json
 
-user_name = raw_input("Enter your ems user name:\n")
-password = getpass.getpass("Enter your login password:\n")
+home_dir = os.path.expanduser('~')
+credential_dir = os.path.join(home_dir, '.credentials')
+ems_ps_file = credential_dir + "/ems_ps.json"
 
-data_extraction.login(user_name, password)
+if not os.path.isfile(ems_ps_file):
+    user_name = raw_input("Enter your ems user name:\n")
+    password = getpass.getpass("Enter your login password:\n")
+    data_extraction.login(user_name, password)
+    ps = {
+        'user': user_name,
+        'ps': password
+    }
+    with open(ems_ps_file, 'w') as f:
+        json.dump(ps, f)
+else:
+    use_local = raw_input("Do you want to use existing credential? at {} (y/n)\n".format(ems_ps_file))
+    if use_local.lower() == 'n':
+        user_name = raw_input("Enter your ems user name:\n")
+        password = getpass.getpass("Enter your login password:\n")
+        data_extraction.login(user_name, password)
+        ps = {
+            'user': user_name,
+            'ps': password
+        }
+        with open(ems_ps_file, 'w') as f:
+            json.dump(ps, f)
+    else:
+        with open(ems_ps_file, 'r') as f:
+            ps = json.load(f)
+            user_name = ps['user']
+            password = ps['ps']
+        data_extraction.login(user_name, password)
+
 calendar_adder.add_deadline()
